3b25dd522ef0c13efa1406c6258c552c153cc674
Koichi <cx.w0077@gmail.com>
2016-02-15 22:31:18 +0800

    first commit

--- sampleVideo2/ViewController.swift
+++ sampleVideo2/ViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var recordBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        recordBtn.backgroundColor = UIColor.orangeColor()
+        recordBtn.layer.cornerRadius = 15
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
